14885ba65d0afe5c4f3a6d389c89aba55288ee17
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2020-07-10 17:01:45 +0800

    停機公告。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { shakeAnimation, shakeOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    shakeAnimation(),
+    shakeOnEnterAnimation()
+  ]
 })
 export class AppComponent {
   title = 'oneknow-maintenance';
